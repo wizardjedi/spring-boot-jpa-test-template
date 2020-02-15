cc4c1e963d1580a85d5478d4c2c1127f8a71dd11
Sayapin Alexander <wizardjedi@gmail.com>
2020-02-15 11:38:30 +0300

    Initial

---
+++ src/test/java/com/a1s/testjpa/TestJpaApplicationTests.java
@@ -0,0 +1,21 @@
+package com.a1s.testjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestJpaApplicationTests {
+
+    private static final Logger log = LoggerFactory.getLogger(TestJpaApplicationTests.class);
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
