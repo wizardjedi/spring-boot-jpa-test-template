a831db74668eefbae763ba2f8fa777206207e771
Sayapin Alexander <a.sayapin@a1-systems.com>
2021-02-20 11:09:55 +0300

    Fix test for junit5

--- src/test/java/com/a1s/testjpa/TestJpaApplicationTests.java
+++ src/test/java/com/a1s/testjpa/TestJpaApplicationTests.java
@@ -1,13 +1,10 @@
 package com.a1s.testjpa;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestJpaApplicationTests {
 
